2de3e23fbb95194d7190106944f7c863779501f3
kevinderitis <kevin.deritis@hotmail.com>
2022-09-26 21:47:55 -0300

    ruta /ruta creada

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.send('Hola mundo!')
 })
 
+app.get('/ruta', (req, res) => {
+    res.send('Ruta creada. ')
+})
+
 const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => console.log('Server running'))
